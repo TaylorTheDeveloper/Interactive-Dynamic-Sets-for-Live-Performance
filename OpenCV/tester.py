469b86cc474809006ce75b88a91a3d5ace923738
Jim Bach <imdigitaljim@gmail.com>
2016-02-01 19:50:52 -0500

    effects update, interactive water, fire, arrow point, explosion canisters, particle follow

--- OpenCV/tester.py
+++ OpenCV/tester.py
@@ -13,7 +13,6 @@ import socket
 
 def sendUDP(mess):
     '''message with id at begining'''
-    print "sent ", mess, " to ", UDP_IP,":",UDP_PORT
     sock.sendto(mess , (UDP_IP, UDP_PORT))
 
 UDP_IP = "127.0.0.1"
@@ -23,7 +22,7 @@ UDP_PORT = 5005
 sock = socket.socket(socket.AF_INET, # Internet
                  socket.SOCK_DGRAM) # UDP
 
-fo = open("tests/1","r")
+fo = open("tests/4","r")
 lines = fo.readlines()
 
 while (True):
---
+++ Unity3D/InteractiveDance/Assets/Scripts/CharacterController.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CharacterController : MonoBehaviour {
+
+    Transform obj;
+    public float speed = .5f;
+	// Use this for initialization
+	void Start () {
+        obj = gameObject.GetComponent<Transform>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        
+	    if (Input.GetKey("a"))
+        {
+            obj.position = new Vector3(obj.position.x - speed, obj.position.y, obj.position.z);
+        }
+        if (Input.GetKey("d"))
+        {
+            obj.position = new Vector3(obj.position.x + speed, obj.position.y, obj.position.z);
+        }
+        if (Input.GetKey("w"))
+        {
+            obj.position = new Vector3(obj.position.x, obj.position.y , obj.position.z + speed);
+        }
+        if (Input.GetKey("s"))
+        {
+            obj.position = new Vector3(obj.position.x, obj.position.y , obj.position.z - speed);
+        }
+    }
+}
