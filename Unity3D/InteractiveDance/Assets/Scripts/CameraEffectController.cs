71f25040ad8da0238b85a6d52dc8bb6d92623c84
Jim Bach <imdigitaljim@gmail.com>
2016-02-18 11:21:03 -0500

    effect production additions, gradual camera movements

--- Unity3D/InteractiveDance/Assets/Scripts/CameraEffectController.cs
+++ Unity3D/InteractiveDance/Assets/Scripts/CameraEffectController.cs
@@ -7,6 +7,8 @@ public class CameraEffectController : MonoBehaviour
     private CameraController _camera;
     public bool Is2D = true;
     private bool _lastUpdate = true;
+    public float[] TransitionTime = new float[5];
+    private int _currentTransition;
 	// Use this for initialization
 	void Start () {
 	    _camera = GameObject.Find("Main Camera").GetComponent<CameraController>();
@@ -17,8 +19,13 @@ public class CameraEffectController : MonoBehaviour
 	    if (_lastUpdate != Is2D)
 	    {
 	        _camera.is2D = !_camera.is2D;
-       
 	    }
 	    _lastUpdate = Is2D;
+
+	    if (_currentTransition < TransitionTime.Length && TransitionTime[_currentTransition] < GlobalTimer.RunningTime)
+	    {
+	        _currentTransition++;
+	        _camera.is2D = !_camera.is2D;
+        }
 	}
 }
